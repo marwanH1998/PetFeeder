c7af2d446e21e7686b098f521d8a550e5541e938
MahmoudSultan1 <50178296+MahmoudSultan1@users.noreply.github.com>
2021-11-25 13:11:32 +0200

    added uart2

---
+++ Pet_Feeder/MDK-ARM/Pet_Feeder/heap_4.d
@@ -0,0 +1,13 @@
+pet_feeder\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c
+pet_feeder\heap_4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+pet_feeder\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+pet_feeder\heap_4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+pet_feeder\heap_4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+pet_feeder\heap_4.o: ../Core/Inc/FreeRTOSConfig.h
+pet_feeder\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+pet_feeder\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+pet_feeder\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+pet_feeder\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+pet_feeder\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+pet_feeder\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+pet_feeder\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
---
+++ Pet_Feeder/MDK-ARM/Pet_Feeder/port.d
@@ -0,0 +1,12 @@
+pet_feeder\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/port.c
+pet_feeder\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+pet_feeder\port.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+pet_feeder\port.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+pet_feeder\port.o: ../Core/Inc/FreeRTOSConfig.h
+pet_feeder\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+pet_feeder\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+pet_feeder\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+pet_feeder\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+pet_feeder\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+pet_feeder\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+pet_feeder\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
